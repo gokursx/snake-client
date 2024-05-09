ba0b402a053818985fa56f9a70dbe0d0422ea3db
Gokul Krishna <gokursx.123@gmail.com>
2024-05-09 16:19:59 -0700

    constants.js

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+const upKeY = 'w';
+const leftKEY = 'a';
+const downKEY = 's';
+const rightKEY = 'd';
+
+//EXPORT
+module.exports = {
+  IP,
+  PORT,
+  upKeY,
+  leftKEY,
+  downKEY,
+  rightKEY,
+};
