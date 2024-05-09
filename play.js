51430a6b6a27f40bed02f4fa550ec5b0cb92600d
Gokul Krishna <gokursx.123@gmail.com>
2024-05-09 09:24:34 -0700

    Removed connect function and imported it

--- play.js
+++ play.js
@@ -1,31 +1,9 @@
-const connect = require("/.conn")
+//Importing connect function to module
+const connect = require('./client')
 
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // Enclosed in quotes
-    port: 50541 // Assuming this is your port number; change it if yours is different
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    conn.write("Name: XXX");
-  });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  conn.on("error", (error) => {
-    console.error(error);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
